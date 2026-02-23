3fb6ad4bf1e6727d00fcf9448da232fc7347b45a
dandoetech <oss@dandoe.tech>
2026-02-23 12:54:09 +0100

    update current working draft before refactor with CLAUDE (wip)

--- src/Definition/RelationType.php
+++ src/Definition/RelationType.php
@@ -6,8 +6,11 @@ namespace DanDoeTech\ResourceRegistry\Definition;
 
 enum RelationType: string
 {
-    case BelongsTo = 'belongs_to';
-    case HasMany = 'has_many';
     case HasOne = 'has_one';
+    case HasMany = 'has_many';
+    case BelongsTo = 'belongs_to';
+    case BelongsToMany = 'belongs_to_many';
+    case HasManyThrough = 'has_many_through';
+    case HasOneThrough = 'has_one_through';
     case ManyToMany = 'many_to_many';
 }
